def5557e56d9b6c1b7737f13178401dfb3945479
MattiaCugusi <m.cugusi287@gmail.com>
2024-11-12 10:37:57 +0400

    esercizio completato

--- indovina.php
+++ indovina.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,8 +12,23 @@
     <title>Indovina Numero</title>
 </head>
 <body>
-    
+
     <?php
+    if (!isset($_SESSION["giocate"])){
+        $_SESSION["giocate"] = 0;
+    }
+
+    if (!isset($_SESSION["vittorie"])){
+        $_SESSION["vittorie"] = 0;
+    }
+        
+
+   
+    ?>
+    
+
+    <?php
+    $_SESSION["giocate"]++;
     $numRand = rand(1,20);
     echo "<div style='text-align: center; border: 1px solid black; width: 60%; margin: auto'>";
     echo "<h1>Il numero generato e': " . $numRand . "</h1>";
@@ -16,9 +37,12 @@
 
     if ($numRand == $numIns){
         echo "<h2 style='color: green'>HAI INDOVINATO!!</h2>";
+        $_SESSION["vittorie"]++;
     }else{
         echo "<h2 style='color: red'>HAI SBAGLIATO!!</h2>";
     }
+    echo "<p>Giocate effettuate: " . $_SESSION["giocate"] . "</p>";
+    echo "<p>Vittorie: " . $_SESSION["vittorie"] . "</p>";
     echo "<p><a href='./scelta.html'>ritenta la giocata</a></p>";
     echo "</div>";
     ?>
